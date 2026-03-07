b69085381d691fb96a0634d4bd65cc30078f1019
Irfansyed0611 <syedirfan0611@gmail.com>
2026-03-07 15:03:03 +0000

    added terraform certificate

---
+++ content/certifications/TerraformAssociate/index.md
@@ -0,0 +1,6 @@
+---
+date: '1'
+title: 'Terraform Associate'
+cover: './badge.png'
+url: 'https://www.credly.com/badges/7c95aea4-88d4-4e1f-a59b-51595e9582fe'
+---
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
   siteMetadata: {
     title: 'Syed Irfan',
     description:
-      'Syed Irfan is a Cloud Engineer with expertise in building and occasionally designing infrastructure that is both secure and scalable.',
-    siteUrl: 'http://syed-irfan-personal-portfolio.s3-website-us-west-2.amazonaws.com', // No trailing slash allowed!
-    image: '/og-v3.png', // Path to your image you placed in the 'static' folder
+      'Syed Irfan is a Cloud Engineer with expertise in building and designing infrastructure that is both secure and scalable.',
+    siteUrl: 'https://syed-irfan.com', // No trailing slash allowed!
+    image: '/og-v2.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@IrfanSyed06',
   },
   plugins: [
